d55bfef96d9009a170b2a136fa2dd026cb57ea34
Yraelias <davidflemal@gmail.com>
2023-07-16 18:33:35 +0200

    ID-39 generate component

---
+++ DDYDLS_CineClubFrontEnd/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './main/content/pages/home/home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
