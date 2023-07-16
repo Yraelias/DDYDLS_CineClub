a4750ebcbd338629584ac6da8a81bc63acecc8a3
Yraelias <davidflemal@gmail.com>
2023-07-16 19:29:00 +0200

    ID-40 Add home in Module and change app.component

--- DDYDLS_CineClubFrontEnd/src/app/app.module.ts
+++ DDYDLS_CineClubFrontEnd/src/app/app.module.ts
@@ -3,15 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './main/content/pages/home/home.component';
+import {RouterModule, Routes} from '@angular/router';
 
-
+const appRoutes: Routes = [
+  {
+    path: '**',
+    component: HomeComponent,
+  }];
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
