8c1457cffde6fb782cf56df0b5239f29ac2d4a85
Yraelias <davidflemal@gmail.com>
2023-12-11 10:49:01 +0100

    ID-97 Hide User not for movie if not connected and change for Details movie

--- DDYDLS_CineClubFrontEnd/src/app/main/content/pages/movie/details/movie-details.component.ts
+++ DDYDLS_CineClubFrontEnd/src/app/main/content/pages/movie/details/movie-details.component.ts
@@ -11,18 +11,24 @@ import { TMDBMovie, Result } from 'src/app/models/tmdbmovie';
   styleUrls: ['./movie-details.component.css']
 })
 export class MovieDetailsComponent implements OnInit {
-  id_User:number
+  id_User:any  = 1
   movie : Movie = {id_movie : 1, name : '', id_studio : 0, year:0, synopsis : '', rating:{ id_movie :0, id_user:0,id_rating:0,date : new Date, ratings : 0}, avgRating:0 ,ratingForUser:0 };
   tmdbmovie!: TMDBMovie;
   result !: Result; 
   URLimg:any = "https://image.tmdb.org/t/p/w500//gEU2QniE6E77NI6lCU6MxlNBvIx.jpg";
+  isConnected:boolean
 
   constructor(private movie_service : MovieService, private activedRoute : ActivatedRoute) {
-    this.id_User  = activedRoute.snapshot.params['id'];
+    this.movie.id_movie  = activedRoute.snapshot.params['id'];
    }
 
   ngOnInit(): void {
     this.loadMovie();
+    if (sessionStorage.getItem('isConnected'))
+    {
+      this.isConnected = true;
+      this.id_User = sessionStorage.getItem('id');
+    }
   }
 
   loadMovie():void{
