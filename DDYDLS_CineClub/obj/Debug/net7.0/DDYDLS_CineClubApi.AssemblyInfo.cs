8f33c7629d9d1cf3715867277ac30bc84b9b398a
Yraelias <davidflemal@gmail.com>
2024-04-10 18:22:57 +0200

    Change Dockerfile for 5.0 to 7.0

--- DDYDLS_CineClub/obj/Debug/net7.0/DDYDLS_CineClubApi.AssemblyInfo.cs
+++ DDYDLS_CineClub/obj/Debug/net7.0/DDYDLS_CineClubApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DDYDLS_CineClubApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16d5aa04f42193bcb431200059f0cf5175787a66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e54d9d1f5829759fa2a5424b5a289a7a6dc9752")]
 [assembly: System.Reflection.AssemblyProductAttribute("DDYDLS_CineClubApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DDYDLS_CineClubApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
